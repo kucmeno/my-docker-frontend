c772188c8a5a952e625f543f926f5ed1fc19abb8
Blazej <kucmeno12@o2.pl>
2020-04-02 16:25:42 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn now !!!!!!!!!!!!!!!!!!!!!!!1
+          Learn React now :)
         </a>
       </header>
     </div>
